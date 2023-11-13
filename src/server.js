4897569a391c84a6b41683e98e4be756375a00a7
Shahrizan <shahrizan.learner@gmail.com>
2023-11-13 11:27:08 +0700

    first commit local

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import Hapi from '@hapi/hapi';
+import routes from './routes.js';
+
+const init = async () => {
+  const server = Hapi.server({
+    host: 'localhost',
+    port: 5050,
+    routes: {
+      cors: true,
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
