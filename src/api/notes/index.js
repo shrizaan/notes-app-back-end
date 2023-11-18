91aa2ed13ab5792006c245e9c1071bfb3b2d2c45
Shahrizan <shahrizan.learner@gmail.com>
2023-11-18 17:39:09 +0700

    restructure with hapi plugin

---
+++ src/api/notes/index.js
@@ -0,0 +1,11 @@
+import routes from './routes.js';
+import NotesHandler from './handler.js';
+
+export default {
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, { service }) => {
+    const notesHandler = new NotesHandler(service);
+    server.route(routes(notesHandler));
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 import Hapi from '@hapi/hapi';
-import routes from './routes.js';
+import notes from './api/notes/index.js';
+import NotesServices from './services/inMemory/NotesServices.js';
 
 const init = async () => {
   const server = Hapi.server({
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    port: 5000,
+    port: 3000,
     routes: {
       cors: true,
     },
   });
 
-  server.route(routes);
+  const notesService = new NotesServices();
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
