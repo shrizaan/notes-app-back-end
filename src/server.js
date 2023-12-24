0606b233d77be834d5f224eb5f3eb65088add815
Shahrizan <shahrizan.learner@gmail.com>
2023-12-24 13:40:47 +0700

    local storage implemented

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
 const path = require('path');
 
 // notes
@@ -57,6 +58,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
