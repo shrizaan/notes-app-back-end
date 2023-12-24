412d84eb7858032b41d9dbd22d1a6c329a36928d
Shahrizan <shahrizan.learner@gmail.com>
2023-12-24 13:17:19 +0700

    add plugin uploads and install inert package

--- src/services/storage/StorageService.js
+++ src/services/storage/StorageService.js
@@ -4,17 +4,17 @@ class StorageService {
   constructor(folder) {
     this._folder = folder;
     
-    if (fs.existsSync(folder)) {
+    if (!fs.existsSync(folder)) {
       fs.mkdirSync(folder, { recursive: true });
     }
   }
   
-  async writeFile(file, meta) {
+  writeFile(file, meta) {
     const filename = +new Date() + meta.filename;
     const path = `${this._folder}/${filename}`;
-    
+ 
     const fileStream = fs.createWriteStream(path);
-    
+ 
     return new Promise((resolve, reject) => {
       fileStream.on('error', (error) => reject(error));
       file.pipe(fileStream);
